b7d1f2d3ddfb9464aa8e7bfb9c51ca1c99520683
Harvey Xie <Harvey_xie@hotmail.com>
2022-07-24 12:38:31 -0400

    edit api url

--- api/main.py
+++ api/main.py
@@ -34,12 +34,12 @@ CORS(app)
 app.config["DEBUG"] = DEBUG
 
 
-@app.route("/")
+@app.route("/api/home")
 def home():
     return {"response": "Welcome to the API"}
 
 
-@app.route("/new-image")
+@app.route("/api/new-image")
 def new_image():
     word = request.args.get("query")
     headers = {"Accept-Version": "V1", "Authorization": "Client-ID " + UNSPLASH_KEY}
@@ -50,7 +50,7 @@ def new_image():
     return data
 
 
-@app.route("/images", methods=["GET", "POST"])
+@app.route("/api/images", methods=["GET", "POST"])
 def images():
     if request.method == "GET":
         # read images from the database
@@ -66,7 +66,7 @@ def images():
         return {"inserted_id": inserted_id}
 
 
-@app.route("/images/<image_id>", methods=["DELETE"])
+@app.route("/api/images/<image_id>", methods=["DELETE"])
 def images_delete(image_id):
     if request.method == "DELETE":
         # delete image from the database
@@ -78,7 +78,7 @@ def images_delete(image_id):
         return {"deleted_id": image_id}
 
 
-@app.route("/stock_price", methods=["GET"])
+@app.route("/api/stock_price", methods=["GET"])
 def stock_price():
     # FMP_HIST_URL = "https://financialmodelingprep.com/api/v3/historical-price-full/"
     ticker = request.args.get("ticker")
