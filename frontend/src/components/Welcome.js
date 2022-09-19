b8788c90104e4c682f10a54741253c7e27105930
Harvey Xie <Harvey_xie@hotmail.com>
2022-09-18 20:20:09 -0400

    remove Jumbotron due to bootstrap 5

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Button, Jumbotron } from 'react-bootstrap';
 
 const Welcome = () => (
-  <Jumbotron>
-    <h1>Harvey Xie's personal Site</h1>
-    <p>This is simple application using React and Python.</p>
-    <h5>Current functionalities (via sidebar):</h5>
-    <li>Stock Perfromance: W.I.P. performance analytics</li>
-    <li>Image Search: search random picture and store in MangoDB</li>
-    <Button
-      variant="primary"
-      href="https://github.com/krocellx/personal_site_react"
-      target="_blank"
-      style={{ marginTop: '6px' }}
-    >
-      GitHub Link
-    </Button>
-  </Jumbotron>
+  <div class="container-fluid bg-light text-dark p-5">
+    <div class="container bg-light p-5">
+      <h1>Harvey Xie's personal Site</h1>
+      <p>This is simple application using React and Python.</p>
+      <h5>Current functionalities (via sidebar):</h5>
+      <li>Stock Perfromance: W.I.P. performance analytics</li>
+      <li>Image Search: search random picture and store in MangoDB</li>
+      <Button
+        variant="primary"
+        href="https://github.com/krocellx/personal_site_react"
+        target="_blank"
+        style={{ marginTop: '6px' }}
+      >
+        GitHub Link
+      </Button>
+    </div>
+  </div>
 );
 
 export default Welcome;
