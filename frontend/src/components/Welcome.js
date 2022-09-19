8496379c8365b0d34f01463a4f73685c93799579
Harvey Xie <Harvey_xie@hotmail.com>
2022-09-19 00:33:39 -0400

    Revert "remove Jumbotron due to bootstrap 5"
    This reverts commit b8788c90104e4c682f10a54741253c7e27105930.

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Button, Jumbotron } from 'react-bootstrap';
 
 const Welcome = () => (
-  <div class="container-fluid bg-light text-dark p-5">
-    <div class="container bg-light p-5">
-      <h1>Harvey Xie's personal Site</h1>
-      <p>This is simple application using React and Python.</p>
-      <h5>Current functionalities (via sidebar):</h5>
-      <li>Stock Perfromance: W.I.P. performance analytics</li>
-      <li>Image Search: search random picture and store in MangoDB</li>
-      <Button
-        variant="primary"
-        href="https://github.com/krocellx/personal_site_react"
-        target="_blank"
-        style={{ marginTop: '6px' }}
-      >
-        GitHub Link
-      </Button>
-    </div>
-  </div>
+  <Jumbotron>
+    <h1>Harvey Xie's personal Site</h1>
+    <p>This is simple application using React and Python.</p>
+    <h5>Current functionalities (via sidebar):</h5>
+    <li>Stock Perfromance: W.I.P. performance analytics</li>
+    <li>Image Search: search random picture and store in MangoDB</li>
+    <Button
+      variant="primary"
+      href="https://github.com/krocellx/personal_site_react"
+      target="_blank"
+      style={{ marginTop: '6px' }}
+    >
+      GitHub Link
+    </Button>
+  </Jumbotron>
 );
 
 export default Welcome;
