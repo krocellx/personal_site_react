a53c3e07c2f0da7b22e83dd714f923c715a21cce
Harvey Xie <Harvey_xie@hotmail.com>
2022-10-10 17:26:56 -0400

    update data transform

--- api/utility.py
+++ api/utility.py
@@ -4,7 +4,7 @@ import json
 
 def transform_fundamental_list_to_dict(ls_fundamental: list) -> dict:
     df_fundamental = pd.DataFrame(ls_fundamental).iloc[::-1]
-    dict_fundamental = df_fundamental.to_dict("list")
+    dict_fundamental = df_fundamental.to_dict("records")
     return dict_fundamental
 
 
