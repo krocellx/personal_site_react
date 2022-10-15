ec60b65e73e6ea9fafa35bb868a1a00cc090f410
Harvey Xie <Harvey_xie@hotmail.com>
2022-10-15 12:05:53 -0400

    update jupyter page

--- frontend/src/pages/ReplicationFactorInvestingUsingCMAs.js
+++ frontend/src/pages/ReplicationFactorInvestingUsingCMAs.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Container, iframe } from 'react-bootstrap';
-import Iframe from 'react-iframe';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050/api';
 
 const ReplicationFactorInvestingUsingCMAs = () => {
+  // const iframe = document.getElementById('my_iframe').contentDocument;
+  // const handleStockSearchSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     // setLoading(true);
+  //     const res = await axios.get(
+  //       `${API_URL}/replication-factor-investing-using-cmas`
+  //     );
+  //     iframe.write(res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <iframe
       title="test"
+      id="my_iframe"
       class="responsive-iframe"
-      src="%PUBLIC_URL%/Replication_Factor_Investing_Using_CMAs.html"
+      src={`${API_URL}/replication-factor-investing-using-cmas`}
       width="100%"
       height="100%"
     ></iframe>
